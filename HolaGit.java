75e5056d136c88efd63acb8139eb18ccbbde770f
julen <julenartesona@gmail.com>
2025-11-08 19:16:40 +0100

    Primer commit: archivo HolaGit.java creado

---
+++ HolaGit.java
@@ -0,0 +1,6 @@
+public class HolaGit {
+    public static void main(String[] args) {
+        System.out.println("¡Hola desde Git!");
+        System.out.println("Proyecto creado por el equipo.");
+    }
+}
