09302dd3d89d67c0b007a931e38f8a2ac28c859b
julen <julenartesona@gmail.com>
2025-11-09 19:23:26 +0100

    añadido mensaje de javi

--- HolaGit.java
+++ HolaGit.java
@@ -2,5 +2,6 @@ public class HolaGit {
     public static void main(String[] args) {
         System.out.println("¡Hola desde Git!");
         System.out.println("Proyecto creado por el equipo.");
+	System.out.println("Mensaje de javi solo juega al rust")
     }
 }
